21d376873846231739a04f860ff84fca40724a2a
kimminkyu <kmk50009@naver.com>
2020-11-19 12:22:43 +0900

    update1

--- Step03_Operator/src/test/main/MainClass03.java
+++ Step03_Operator/src/test/main/MainClass03.java
@@ -14,6 +14,7 @@ public class MainClass03 {
 		boolean result6=10<=10;//true
 		
 		// String type 변수에 null 대입하기 
+		// null은 참조데이터!! type이 담길 수 있는 빈 공간을 만들때 사용한다.
 		String name=null;
 		
 		//어떤 값이 null 인지 아닌지 비교 가능하다 
---
+++ Step03_Operator/src/test/main/MainClass06.java
@@ -0,0 +1,30 @@
+package test.main;
+/*
+ *  6. 3항 연산자 테스트
+ * 
+ * 
+ */
+
+public class MainClass06 {
+	public static void main(String[] args) {
+		boolean isWait=false;
+		
+		//isWait이 트루이면 "기다려요", 거짓이면 "기다리지않아요"
+		//둘중에 하나의 결과를 원할때 사용함.
+		//1번
+		String result=isWait? "기다려요" : "기다리지 않아요";
+		System.out.println(result);
+		
+		//2번 (1번과 2번 같은 로직임)
+		String result2=null;
+		if(isWait) {
+			result2="기다려요";
+		}else {
+			result2="기다리지 않아요";
+		}
+		System.out.println(result2);
+		
+		//10.12f이면 float이지만 생략되었으므로 double
+	    double	result3=isWait ? 10.12 : 99.5;
+	}
+}
