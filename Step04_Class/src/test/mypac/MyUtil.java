2bd998de11a0dee21b91ac9e27d5fb03f73ed323
kimminkyu <kmk50009@naver.com>
2020-11-20 17:26:47 +0900

    up6

--- Step04_Class/src/test/mypac/MyUtil.java
+++ Step04_Class/src/test/mypac/MyUtil.java
@@ -3,10 +3,18 @@ package test.mypac;
  *  static 자원을 포함하는 용도로 사용해 보기
  */
 public class MyUtil {
+	//non static 필드
+	public int version;
 	
+	//static 필드
 	public static String owner;
 	
 	public static void playMusic() {
 		System.out.println("음악을 틀어요!");
+		//static 안에서 non static 참조 불가.
+		//int a=this.version;
+		//int b=MyUtil.version;
+		
+		String c=MyUtil.owner;
 	}
 }
