4d411344928c69e5126759026be5e58b2427dc59
kimminkyu <kmk50009@naver.com>
2020-12-11 12:22:21 +0900

    up54

---
+++ Step19_Socket/src/example2/ClientMain.java
@@ -0,0 +1,35 @@
+package example2;
+
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientMain {
+	public static void main(String[] args) {
+		//1.Socket을 통해서 출력할 전송할 문자열을 콘솔로 입력받아서
+		Scanner scan=new Scanner(System.in);
+		System.out.println("전송할 문자열 입력 :");
+		String msg=scan.nextLine();
+		
+		Socket socket=null;
+		try {
+			//new Socket("접속할 ip주소", 포트번호)
+			//127.0.0.1 = 나의아이피주소를 부를때 사용 this랑 비슷한느낌
+			socket=new Socket("127.0.0.1", 5000); 
+			System.out.println("서버에 Socket 접속 성공!");
+			//2.Socket을 통해서 출력하기
+			OutputStream os=socket.getOutputStream();
+			OutputStreamWriter osw=new OutputStreamWriter(os);
+			osw.write(msg);//입력한 문자열 출력
+			osw.write("\r\n"); //개행기호출력
+			osw.flush();//방출
+			osw.close();//닫아주기
+			
+			socket.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Step19_Socket/src/example2/ServerMain.java
@@ -0,0 +1,43 @@
+package example2;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+	public static void main(String[] args) {
+		
+		ServerSocket serverSocket=null;
+		try {
+			//5000번 통신 port를 열고 클라이언트의 접속을 기다린다.
+			serverSocket=new ServerSocket(5000);
+			/*
+			 * .accept() 메소드는 클라이언트가 접속을 해야지 리턴하는 메소드이다.
+			 * 클라이언트가 접속을 해오면 해당 클라이언트와 연결된Socket 객체의
+			 * 참조값을 리턴한다. 
+			 */
+			while(true) {
+				System.out.println("클라이언트의 Socket 접속을 기다립니다...");
+				Socket socket=serverSocket.accept();
+				System.out.println("클라이언트가 접속을 했습니다.");
+				String ip=socket.getInetAddress().getHostAddress();
+				System.out.println("접속한 클라이언트의 ip 주소 : "+ip);
+				//방금 접속을 한 클라이언트로 부터 입력받을 객체의 참조값
+				InputStream is=socket.getInputStream();
+				InputStreamReader isr=new InputStreamReader(is);
+				BufferedReader br=new BufferedReader(isr);
+				String msg=br.readLine(); //한줄을 읽어온다 한줄의 기준? 개행기호! 그래서 클라이언트에서 보낼때 개행기호("\r\n") 같이 보내준거임.
+				System.out.println("클라이언트가 전송한 문자열 : "+msg);
+				
+				socket.close();
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("ServerMain main 메소드가 종료됩니다.");
+	}
+}
+	
