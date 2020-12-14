01622de6d9017a5cf3657c9acf173c70dead07c2
kimminkyu <kmk50009@naver.com>
2020-12-14 10:39:23 +0900

    up56

---
+++ Step19_Socket/src/example5/ClientFrame.java
@@ -0,0 +1,89 @@
+package example5;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ClientFrame extends JFrame implements ActionListener{
+	JTextField tf;
+	//생성자
+	public ClientFrame(String title) {
+		super(title);
+		setLayout(new BorderLayout());
+		
+		
+		//페널을 만들어서 프레임의 위쪽에 배치하기
+		JPanel topPanel=new JPanel();
+		topPanel.setBackground(Color.YELLOW);
+		add(topPanel, BorderLayout.NORTH);
+		
+		//프레임에 UI 배치 하기
+		tf=new JTextField(50);
+		JButton sendBtn=new JButton("send");
+		topPanel.add(tf);
+		topPanel.add(sendBtn);
+		
+		//버튼 리스너 등록하기
+		sendBtn.addActionListener(this);
+		
+	
+		
+	}
+	public static void main(String[] args) {
+		//프레임객체 생성하면서 프레임의 제목을 chatting 으로 지정하기
+		ClientFrame f=new ClientFrame("chatting");
+		//프레임의 x좌표, y좌표, width, height 설정하기
+		f.setBounds(100, 100, 800, 200);
+		//프레임 닫을때 프로세스도 종료 되도록한다.
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//프레임을 화면상에 보이게 하기
+		f.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//1. JTextField 에 입력한 문자열을 읽어와서
+		String msg=tf.getText();
+		//2. Socket 접속을 해서 전송한다.
+		Socket socket=null;
+		try {
+			//new Socket("접속할 ip주소", 포트번호)
+			//127.0.0.1 = 나의아이피주소를 부를때 사용 this랑 비슷한느낌
+			socket=new Socket("127.0.0.1", 5000); 
+			System.out.println("서버에 Socket 접속 성공!");
+			//2.Socket을 통해서 출력하기
+			OutputStream os=socket.getOutputStream();
+			OutputStreamWriter osw=new OutputStreamWriter(os);
+			osw.write(msg);//입력한 문자열 출력
+			osw.write("\r\n"); //개행기호출력
+			osw.flush();//방출
+			
+			
+			//3.Socket을 통해서 입력받기
+			InputStream is=socket.getInputStream();
+			InputStreamReader isr=new InputStreamReader(is);
+			BufferedReader br=new BufferedReader(isr);
+			//서버가 전송한 문자열 읽어들이기
+			String line=br.readLine();
+			System.out.println(line);
+			
+			socket.close();
+			
+		}catch(Exception e1) {
+			e1.printStackTrace();
+		}
+		//3. JTextField에 입력한 문자열 삭제
+		tf.setText("");
+	}
+}
---
+++ Step19_Socket/src/example5/ServerMain.java
@@ -0,0 +1,56 @@
+package example5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+	public static void main(String[] args) {
+		
+		ServerSocket serverSocket=null;
+		try {
+			//5000번 통신 port를 열고 클라이언트의 접속을 기다린다.
+			serverSocket=new ServerSocket(5000);
+			/*
+			 * .accept() 메소드는 클라이언트가 접속을 해야지 리턴하는 메소드이다.
+			 * 클라이언트가 접속을 해오면 해당 클라이언트와 연결된Socket 객체의
+			 * 참조값을 리턴한다. 
+			 */
+			while(true) {
+				System.out.println("클라이언트의 Socket 접속을 기다립니다...");
+				Socket socket=serverSocket.accept();
+				System.out.println("클라이언트가 접속을 했습니다.");
+				String ip=socket.getInetAddress().getHostAddress();
+				System.out.println("접속한 클라이언트의 ip 주소 : "+ip);
+				//방금 접속을 한 클라이언트로 부터 입력받을 객체의 참조값
+				InputStream is=socket.getInputStream();
+				InputStreamReader isr=new InputStreamReader(is);
+				BufferedReader br=new BufferedReader(isr);
+				String msg=br.readLine(); //한줄을 읽어온다 한줄의 기준? 개행기호! 그래서 클라이언트에서 보낼때 개행기호("\r\n") 같이 보내준거임.
+				System.out.println("클라이언트가 전송한 문자열 : "+msg);
+				
+				//클라이언트에게 출력할 수 있는 객체의 참조값
+				OutputStream os=socket.getOutputStream();
+				OutputStreamWriter osw=new OutputStreamWriter(os);
+				BufferedWriter bw=new BufferedWriter(osw);
+				bw.write("안녕 클라이언트 난 서버야~");
+				bw.newLine();//개행기호를 메소드를 이용해서 출력할 수 있다.
+				bw.flush();
+				bw.close();
+				
+				
+				socket.close();
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("ServerMain main 메소드가 종료됩니다.");
+	}
+}
+	
